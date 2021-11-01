ced24d1cbd095c7ef6bb6356b5d86817a5db1ce9
Pham Hung <pham.hung.061020@gmail.com>
2021-11-01 09:24:39 +0700

    gan xong homepage

---
+++ resources/views/User/layout/footer.blade.php
@@ -0,0 +1,63 @@
+<!-- Footer -->
+<footer class="footer">
+  <div class="footer-top">
+    <div class="container">
+      <div class="footer-item">
+        <h4 class="footer-title">Giới thiệu</h4>
+        <div class="footer-content">
+          <p>
+            G2 SHOES NƠI GIAO LƯU VÀ HỌC HỎI MUA BÁN CÁC SẢN PHẨM CHÍNH
+            HÃNG.
+          </p>
+        </div>
+      </div>
+      <div class="footer-item">
+        <h4 class="footer-title">Liên kết</h4>
+        <div class="footer-content">
+          <ul class="footer-list">
+            <li class="footer-item">
+              <a href="#" class="footer-link">Tìm kiếm</a>
+            </li>
+            <li class="footer-item">
+              <a href="#" class="footer-link">Chính sách đổi trả</a>
+            </li>
+            <li class="footer-item">
+              <a href="#" class="footer-link">Chính sách bảo hành</a>
+            </li>
+            <li class="footer-item">
+              <a href="#" class="footer-link">Giao hàng tận nơi</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="footer-item">
+        <h4 class="footer-title">Thông tin liên hệ</h4>
+        <div class="footer-content">
+          <ul class="footer-list">
+            <li class="footer-item">
+              <span class="footer-link">Địa chỉ: </span>
+              Số 298 đường Cầu Diễn, quận Bắc Từ Liêm, Hà Nội
+            </li>
+            <li class="footer-item">
+              <span class="footer-link">Điện thoại: </span>0243 765 5121
+            </li>
+            <li class="footer-item">
+              <span class="footer-link">Instagram: </span>G2SHOES
+            </li>
+            <li class="footer-item" style="margin-bottom: 24px">
+              <span class="footer-link">Mail: </span
+                >Nhom2pmmnm@gmail.com
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      <p>
+        Copyright &copy; 2021
+        <a href="#">G2 SHOES</a>
+      </p>
+    </div>
+  </footer>
+<!-- Footer -->
