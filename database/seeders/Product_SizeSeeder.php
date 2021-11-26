ffcee280a7d274ddc4c91171fca6bf49e7544188
Lê Vũ Long <01628026493long@gmail.com>
2021-11-26 14:58:25 +0700

    add seeders

--- database/seeders/Product_SizeSeeder.php
+++ database/seeders/Product_SizeSeeder.php
@@ -293,5 +293,425 @@ class Product_SizeSeeder extends Seeder
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s")
         ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '1',
+            'product_id' => '21',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '2',
+            'product_id' => '21',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '3',
+            'product_id' => '22',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '4',
+            'product_id' => '22',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '5',
+            'product_id' => '23',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '6',
+            'product_id' => '23',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '7',
+            'product_id' => '24',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '8',
+            'product_id' => '24',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '9',
+            'product_id' => '25',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '10',
+            'product_id' => '25',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '11',
+            'product_id' => '26',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '12',
+            'product_id' => '26',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '13',
+            'product_id' => '27',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '14',
+            'product_id' => '27',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '15',
+            'product_id' => '28',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '16',
+            'product_id' => '28',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '17',
+            'product_id' => '29',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '18',
+            'product_id' => '29',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '19',
+            'product_id' => '30',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '20',
+            'product_id' => '30',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '17',
+            'product_id' => '31',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '21',
+            'product_id' => '31',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '1',
+            'product_id' => '32',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '2',
+            'product_id' => '32',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '3',
+            'product_id' => '33',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '4',
+            'product_id' => '33',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '5',
+            'product_id' => '34',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '6',
+            'product_id' => '34',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '7',
+            'product_id' => '35',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '8',
+            'product_id' => '35',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '9',
+            'product_id' => '36',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '10',
+            'product_id' => '36',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '11',
+            'product_id' => '37',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '12',
+            'product_id' => '37',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '13',
+            'product_id' => '38',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '14',
+            'product_id' => '38',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '15',
+            'product_id' => '39',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '16',
+            'product_id' => '39',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '17',
+            'product_id' => '40',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        // DB::table('product_sizes')->insert([
+        //     'size_id' => '18',
+        //     'product_id' => '40',
+        //     'amount' => '200',
+        //     'created_at' => date("Y-m-d H:i:s"),
+        //     'updated_at' => date("Y-m-d H:i:s")
+        // ]);
+        // DB::table('product_sizes')->insert([
+        //     'size_id' => '19',
+        //     'product_id' => '41',
+        //     'amount' => '100',
+        //     'created_at' => date("Y-m-d H:i:s"),
+        //     'updated_at' => date("Y-m-d H:i:s")
+        // ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '20',
+            'product_id' => '41',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '2',
+            'product_id' => '42',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '3',
+            'product_id' => '42',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '3',
+            'product_id' => '43',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '4',
+            'product_id' => '43',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '17',
+            'product_id' => '44',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '18',
+            'product_id' => '44',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '10',
+            'product_id' => '45',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        // DB::table('product_sizes')->insert([
+        //     'size_id' => '15',
+        //     'product_id' => '45',
+        //     'amount' => '200',
+        //     'created_at' => date("Y-m-d H:i:s"),
+        //     'updated_at' => date("Y-m-d H:i:s")
+        // ]);
+        // DB::table('product_sizes')->insert([
+        //     'size_id' => '8',
+        //     'product_id' => '46',
+        //     'amount' => '100',
+        //     'created_at' => date("Y-m-d H:i:s"),
+        //     'updated_at' => date("Y-m-d H:i:s")
+        // ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '9',
+            'product_id' => '46',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        // DB::table('product_sizes')->insert([
+        //     'size_id' => '16',
+        //     'product_id' => '47',
+        //     'amount' => '100',
+        //     'created_at' => date("Y-m-d H:i:s"),
+        //     'updated_at' => date("Y-m-d H:i:s")
+        // ]);
+        // DB::table('product_sizes')->insert([
+        //     'size_id' => '18',
+        //     'product_id' => '47',
+        //     'amount' => '200',
+        //     'created_at' => date("Y-m-d H:i:s"),
+        //     'updated_at' => date("Y-m-d H:i:s")
+        // ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '13',
+            'product_id' => '48',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '14',
+            'product_id' => '48',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        // DB::table('product_sizes')->insert([
+        //     'size_id' => '4',
+        //     'product_id' => '49',
+        //     'amount' => '100',
+        //     'created_at' => date("Y-m-d H:i:s"),
+        //     'updated_at' => date("Y-m-d H:i:s")
+        // ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '5',
+            'product_id' => '49',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '1',
+            'product_id' => '50',
+            'amount' => '100',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('product_sizes')->insert([
+            'size_id' => '2',
+            'product_id' => '50',
+            'amount' => '200',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
     }
 }
